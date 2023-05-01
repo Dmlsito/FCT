7d0b279afdda58b79284c87c8de2b5f30e907378
daniMallo <101351451+Dmlsito@users.noreply.github.com>
2023-05-01 18:00:40 +0200

    CardBodys con info de fechas

--- development/vite-project/src/components/CardState.jsx
+++ development/vite-project/src/components/CardState.jsx
@@ -1,7 +1,11 @@
 export const CardState = ({ state }) => {
+  const date = new Date(state.LastTimeUsed)
   return (
     <main className='card-body'>
-      <header className='card-header'>DATE</header>
+      <header className='card-header'>
+        <span>{`${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`}</span>
+        <span>{`${date.getUTCHours()}:${date.getMinutes()}:${date.getSeconds()}`}</span>
+      </header>
       <article className='card-article'>{
           state.state === 0
             ? <span className='card-article-text'>machine {state.id}: <span className='card-article-text-free'> free</span></span>
@@ -11,3 +15,5 @@ export const CardState = ({ state }) => {
     </main>
   )
 }
+
+// `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}/${date.getUTCHours()}:${date.getMinutes()}:${date.getSeconds()}` //
